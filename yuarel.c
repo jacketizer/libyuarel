f0f4654f2b41084afc20b141eab636b39221f82b
Ali H. Fardan <raiz@firemail.cc>
2016-08-12 13:26:42 +0300

    use the standard idioms && more corrections

--- yuarel.c
+++ yuarel.c
@@ -61,65 +61,61 @@ yuarel_parse(struct yuarel *url, char *u)
 	memset(url, 0, sizeof (struct yuarel));
 
 	/* relative URL, parse scheme and hostname */
-	if ('/' != *u) {
+	if (*u != '/') {
 		/* Scheme */
 		url->scheme = u;
 		u = strchr(u, ':');
-		if (NULL == u || url->scheme == u) {
+		if (u == NULL || url->scheme == u)
 			return -1;
-		}
 		*(u++) = '\0'; // Replace ':' with NULL
 
 		/* Forward to after // */
-		while ('/' == *u) u++;
+		while (*u == '/') u++;
 
 		/* Host */
-		if ('\0' == *u) {
+		if (*u == '\0')
 			return -1;
-		}
 		url->host = u;
 		start = u;
 	}
 
 	/* (Fragment) */
 	u = strchr(start, '#');
-	if (NULL != u) {
+	if (u != NULL) {
 		*u = '\0';
 		url->fragment = u + 1;
 	}
 
 	/* (Query) */
 	u = strchr(start, '?');
-	if (NULL != u) {
+	if (u != NULL) {
 		*u = '\0';
 		url->query = u + 1;
 	}
 
 	/* (Path) */
 	u = strchr(start, '/');
-	if (NULL != u) {
+	if (u != NULL) {
 		*u = '\0';
 		url->path = u + 1;
 	}
 
 	/* relative URI, parse port */
-	if (NULL != url->scheme) {
+	if (url->scheme != NULL) {
 		/* (Port) */
 		u = strchr(start, ':');
-		if (NULL != u && (NULL == url->path || u < url->path)) {
+		if (u != NULL && (url->path == NULL || u < url->path)) {
 			*(u++) = '\0';
-			if ('\0' == *u) {
+			if (*u == '\0')
 				return -1;
-			}
 
-			if (url->path) {
+			if (url->path)
 				url->port = natoi(u, url->path - u - 1);
-			} else {
+			else
 				url->port = atoi(u);
-			}
 		}
 		/* Missing hostname */
-		if ('\0' == *url->host)
+		if (*url->host == '\0')
 			return -1;
 	}
 
@@ -144,17 +140,15 @@ yuarel_split_path(char *path, char **parts, int max_parts)
 
 	do {
 		/* Forward to after slashes */
-		while ('/' == *path) path++;
+		while (*path == '/') path++;
 
-		if ('\0' == *path) {
+		if (*path == '\0')
 			break;
-		}
 
 		parts[i++] = path;
 
-		if ((path = strchr(path, '/')) == NULL) {
+		if ((path = strchr(path, '/')) == NULL)
 			break;
-		}
 
 		*(path++) = '\0';
 	} while (i < max_parts);
@@ -167,7 +161,7 @@ yuarel_parse_query(char *query, char delimiter, struct yuarel_param *params, int
 {
 	int i = 0;
 
-	if (NULL == query || '\0' == *query)
+	if (query == NULL || *query == '\0')
 		return -1;
 
 	params[i++].key = query;
@@ -178,15 +172,14 @@ yuarel_parse_query(char *query, char delimiter, struct yuarel_param *params, int
 
 		/* Go back and split previous param */
 		if (i > 0) {
-			if (NULL != (params[i - 1].val = strchr(params[i - 1].key, '='))) {
+			if ((params[i - 1].val = strchr(params[i - 1].key, '=')) != NULL)
 				*(params[i - 1].val)++ = '\0';
-			}
 		}
 		i++;
 	}
 
 	/* Go back and split last param */
-	if (NULL != (params[i - 1].val = strchr(params[i - 1].key, '=')))
+	if ((params[i - 1].val = strchr(params[i - 1].key, '=')) != NULL)
 		*(params[i - 1].val)++ = '\0';
 
 	return i;
